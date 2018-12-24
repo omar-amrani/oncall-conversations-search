27e0370c93f65b42cbd9adede644805f08276798
Omar Amrani <omar.amrani2@mail.dcu.ie>
2018-12-24 16:49:28 +0100

    Fixing migration

--- db/migrate/20181023191359_change_creation_date_type_to_timestamp.rb
+++
@@ -1,9 +0,0 @@
-class ChangeCreationDateTypeToTimestamp < ActiveRecord::Migration[5.1]
-  def up
-    change_column :conversation_parts, :creation_date, :Time
-  end
-  def down
-    change_column :conversation_parts, :creation_date, :string
-  end
-end
-
