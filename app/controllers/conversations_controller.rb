26e2d389f4df2c319a50ef8ffda3964e5f4ce175
Omar Amrani <omar.amrani2@mail.dcu.ie>
2018-12-24 12:07:02 +0100

    removing require user before action for testing

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,7 +2,7 @@ class ConversationsController < ApplicationController
   include Constants
   skip_before_action :verify_authenticity_token
   protect_from_forgery with: :null_session
-  before_action :require_user, only: [:index, :search, :show]
+  #before_action :require_user, only: [:index, :search, :show]
 
 
   def search
