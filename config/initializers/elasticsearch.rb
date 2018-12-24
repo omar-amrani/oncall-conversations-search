4dd84ddd2a3be0678a862ed6df3b95348cf81f96
Omar Amrani <omar.amrani2@mail.dcu.ie>
2018-12-24 14:29:14 +0100

    Fxing eb

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 Searchkick.aws_credentials = {
     access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: "us-east-2"
+    region: "us-west-2"
 }
