248438f852f1549d49d0355d0faec4ad6ff57b4f
Omar Amrani <omar.amrani2@mail.dcu.ie>
2018-12-24 13:06:59 +0100

    Adding ElasticSearch production details

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
 
   def self.find_or_create_from_auth_hash(auth_hash)
-    return user = User.find_by(email: auth_hash.extra.raw_info["email"])
     if user.nil?
       return user = create(email: auth_hash.extra.raw_info["email"], name: auth_hash.extra.raw_info["name"])
     end
-
+    return user = User.find_by(email: auth_hash.extra.raw_info["email"])
   end
 
 
