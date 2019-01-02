7bdff1f7348d998a9c0a9ed6a1d7bc5eab5867a2
AmraniOmar <33460735+AmraniOmar@users.noreply.github.com>
2019-01-02 20:22:28 +0000

    Update 20181204215035_create_users_table.rb

--- db/migrate/20181204215035_create_users_table.rb
+++ db/migrate/20181204215035_create_users_table.rb
@@ -1,8 +1,8 @@
 class CreateUsersTable < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :name
+      t.string :email, :limit => 191
+      t.string :name, :limit => 191
       t.timestamps
 
     end
